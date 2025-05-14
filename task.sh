a09036812c91dfb81839bdba480dfb7614df997c
Kateryna <kateryna.kotyh@gmail.com>
2025-05-14 17:00:18 +0300

    Update

--- task.sh
+++ task.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-source create_user.sql
 # This script is used to backup the database
-mysqldump -u "$DB_USER" -p "$DB_PASSWORD" --no-create-db ShopDB --result-file=backup.sql
-mysqldump -u "$DB_USER" -p "$DB_PASSWORD" --no-create-info ShopDB --result-file=backup_data.sql
-mysql -u "$DB_USER" -p "$DB_PASSWORD" ShopDBReverse < backup.sql
-mysql -u "$DB_USER" -p "$DB_PASSWORD" ShopDBDevelopment < backup_data.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-db ShopDB --result-file=backup.sql
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB --result-file=backup_data.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReverse < backup.sql
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < backup_data.sql
+
